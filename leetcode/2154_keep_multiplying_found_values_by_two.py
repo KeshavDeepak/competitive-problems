821fc142eb5691b10c26a95062baf1a88296dfd5
Keshav Deepak <50632203+KeshavDeepak@users.noreply.github.com>
2025-11-19 15:20:57 +0530

    better solution for 2154

--- leetcode/2154_keep_multiplying_found_values_by_two.py
+++ leetcode/2154_keep_multiplying_found_values_by_two.py
@@ -2,34 +2,9 @@ import math
 
 class Solution:
     def findFinalValue(self, nums: List[int], original: int) -> int:
-        powers = []
+        s = set(nums)
 
-        for num in nums:
-            if num >= original and math.log(num/original, 2) % 1 == 0: # num is a power of original
-                powers.append(num)
+        while original in s:
+            original *= 2
         
-        # sort powers
-        powers.sort()
-
-        print(powers)
-
-        # base cases
-        if len(powers) == 0: return original
-
-        if len(powers) == 1: 
-            return original * 2 if powers[0] == original else original 
-
-        # find biggest consecutive one
-        for index in range(1, len(powers)):
-            curr_value = powers[index]
-            prev_value = powers[index-1]
-
-            if curr_value == prev_value: continue
-
-            if curr_value == prev_value * 2:
-                continue
-            else:
-                return prev_value * 2
-        
-        # if th full array was parsed, biggest number is the answer
-        return powers[-1] * 2
+        return original

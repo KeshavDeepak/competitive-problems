860ac1f26965c619ff193a77e6d31d44e751e95f
Keshav Deepak <50632203+KeshavDeepak@users.noreply.github.com>
2025-12-05 15:31:55 +0530

    one more in gfg

---
+++ gfg/minimize_max_distance_to_gas_station.py
@@ -0,0 +1,46 @@
+import math
+
+class Solution:
+    def minMaxDist(self, stations, k):
+        # binary search for answer
+        # l = min gap
+        # r = max gap
+        # simulate and reduce gaps on all stations 
+        # check if reductions_count <= k -- if it is -- hooray -- try lower
+        # if not -- try higher
+        
+        n = len(stations)
+
+        # ------------ base case
+        if n == 1: return 0
+        
+        # ------------ main case
+        # find left and right
+        left = 0
+        right = -math.inf
+        
+        for i in range(1, n):
+            gap = stations[i] - stations[i-1]
+            
+            right = max(right, gap)
+        
+        # binary search
+        while (right - left) > 1e-6:
+            maximum = (left + right) / 2.0
+            
+            # print(left, right, maximum)
+            
+            # is it possible to achieve max
+            new = 0
+            
+            for i in range(1, n):
+                gap = stations[i] - stations[i-1]
+                
+                new += math.ceil(gap / maximum) - 1
+                
+            if new <= k: # it is possible
+                right = maximum
+            else: # not possible
+                left = maximum
+        
+        return right
